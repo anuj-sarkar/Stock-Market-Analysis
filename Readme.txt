f31936ed01fd0ce7a2d11243c80d8df439a5ebbc
Anuj Sarkar <anujsarkar003@gmail.com>
2023-10-11 21:52:37 +0530

    Initial Commit

---
+++ Readme.txt
@@ -0,0 +1,41 @@
+Numpy:
+pip install numpy
+
+Scikit-learn:
+pip install scikit-learn
+
+Keras:
+pip install keras
+
+plotly:
+pip install plotly
+
+Yahoo finance:
+pip install yfinance
+
+Streamlit:
+pip install streamlit
+
+Datetime:
+pip install DateTime
+
+Pandas datareader:
+pip install pandas-datareader
+
+Matplotlib:
+pip install matplotlib
+
+Pandas:
+pip install pandas
+
+Time:
+pip install TIME-python
+
+Write the commands in the VScode terminal to install the respective module.
+Also install the jupyter notebook add-on from the store if you are interested in the analysis part.
+
+To check the website run the command:
+streamlit run filename.py
+
+Have a look at both the files sma.py and test.py and then choose.
+Internet Connection Required!!
---
+++ main.py
@@ -0,0 +1,138 @@
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as pdata
+from datetime import date
+import streamlit as st
+import yfinance as yf
+from plotly import graph_objs as go
+import plotly.express as px
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+
+start="2015-01-01"
+today=date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Trend Analysis")
+
+stocks=("AAPL","GOOG","MSFT","GME")
+
+
+# data_load_state=st.text("Loading data...")
+
+# data=yf.download(selected_stocks,start,today)
+# data=data.drop(['Adj Close'],axis=1)
+
+# data_load_state.text("Loading data...done!")
+
+st.header("Select Stock Ticker for Analysis")
+selected_stocks=st.selectbox("Select",stocks)
+    
+
+with st.spinner("Loading data ..."):
+    data=yf.download(selected_stocks,start,today)
+    data=data.drop(['Adj Close'],axis=1)
+    time.sleep(2);
+st.success("Loading data ... done!")
+
+tab1,tab2=st.tabs(["📑Data","📈Chart"])
+tab1.subheader("Raw Data")
+tab1.table(data.tail())
+
+data1=data;
+data=data.reset_index()
+
+tab2.subheader('Stock closing price with Time Period Selector')
+fig1 = px.line(data,x='Date',y='Close')
+fig1.update_xaxes(
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label='1m', step='month', stepmode='backward'),
+            dict(count=3, label='3m', step='month', stepmode='backward'),
+            dict(count=6, label='6m',step='month', stepmode='backward'),
+            dict(count=1, label='1y',step='year', stepmode='backward'),
+            dict(step='all')
+        ])
+    )
+)
+fig1.update_traces(line_color='#e52b50')
+tab2.plotly_chart(fig1)
+
+st.subheader("Analysed Data Description")
+st.table(data1.describe().drop(['count','25%','50%','75%'],axis=0))
+
+st.subheader("Time Series Data")
+fig=go.Figure();
+fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='Stock Open'))
+fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='Stock Close'))
+fig.update_layout(xaxis_rangeslider_visible=True,template='plotly_dark')
+st.plotly_chart(fig)
+
+# st.subheader("Time Series Data")
+# fig=go.Figure();
+# fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='Stock Open'))
+# fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='Stock Close'))
+# fig.update_layout(xaxis_rangeslider_visible=True,template='plotly_dark')
+# st.plotly_chart(fig)
+
+st.subheader("Time Series Data with Candlestick")
+figure = go.Figure(data=[go.Candlestick(x=data['Date'],open=data['Open'],high=data['High'],low=data['Low'],close=data['Close'])])
+figure.update_layout(xaxis_rangeslider_visible=True,height=600)
+st.plotly_chart(figure)
+
+
+
+ma100=data.Close.rolling(100).mean()
+ma200=data.Close.rolling(200).mean()
+
+st.subheader("Closing Price vs Time Chart with 100 days Moving Average")
+fig=plt.figure(figsize=(10,5))
+plt.plot(data.Close)
+plt.plot(ma100,'r')
+st.pyplot(fig)
+
+
+st.subheader("Closing Price vs Time Chart with 100 days Moving Average and 200 days Moving Average")
+fig=plt.figure(figsize=(10,5))
+plt.plot(data.Close)
+plt.plot(ma100,'r')
+plt.plot(ma200,'g')
+st.pyplot(fig)
+
+data_training=pd.DataFrame(data['Close'][0:int(len(data)*0.7)])
+data_testing=pd.DataFrame(data['Close'][int(len(data)*0.7):int(len(data))])
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler(feature_range=(0,1))
+data_training_array=scaler.fit_transform(data_training)
+
+model=load_model('keras_model.h5')
+past_100_days=data_training.tail(100)
+final_data=[past_100_days,data_testing]
+final_data=pd.concat(final_data)
+input_data=scaler.fit_transform(final_data)
+x_test=[];
+y_test=[];
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+x_test,y_test=np.array(x_test),np.array(y_test)
+
+y_predicted=model.predict(x_test)
+
+scaler=scaler.scale_
+
+scale_factor=1/scaler[0]
+y_predicted=y_predicted*scale_factor
+y_test=y_test*scale_factor
+
+st.subheader("Original Closing Price vs Predicted Closing Price")
+fig2=plt.figure(figsize=(10,5))
+plt.plot(y_test,'black',label='Original Price')
+plt.plot(y_predicted,'green', label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
